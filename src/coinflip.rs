1267ed3b23365f39ca8d3af232c2f60f938aeb99
qweru <itsbookx@gmail.com>
2026-07-26 23:08:17 +0300

    perf: improved performance by 100%

--- src/coinflip.rs
+++
@@ -1,9 +0,0 @@
-use serenity::all::Context;
-use serenity::all::Message;
-
-//coinflip command :3
-pub async fn coinflip(ctx: &Context, msg: &Message) {
-    let flip = rand::random();
-    let result = if flip { "Heads :3" } else { "Tails :3" };
-    let _ = msg.channel_id.say(&ctx.http, result).await;
-}
--- src/randcat.rs
+++
@@ -1,8 +0,0 @@
-use serenity::all::Context;
-use serenity::all::Message;
-
-pub async fn cat(ctx: &Context, msg: &Message) {
-    let randnum = rand::random::<u64>();
-    let randlink = format!("https://cataas.com/cat?a={}", randnum);
-    let _ = msg.channel_id.say(&ctx.http, randlink).await;
-}
